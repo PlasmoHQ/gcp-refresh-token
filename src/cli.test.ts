2ea6f41c60b53a7dcd768ecc24290e9fab66911a
Louis <6723574+louisgv@users.noreply.github.com>
2022-04-11 23:31:28 -0400

    Tidy up test

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,8 +1,11 @@
 import { expect, test } from "@jest/globals"
 import { execFileSync } from "child_process"
 import { existsSync } from "fs"
+import { rm } from "fs/promises"
 import { join } from "path"
 import { cwd, execPath } from "process"
+
+import { getKeyFilePath } from "~index"
 
 const cliScript = join(cwd(), "dist", "cli.js")
 
@@ -10,16 +13,20 @@ const cliErrorRun = () => {
   try {
     execFileSync(execPath, [cliScript]).toString("utf-8")
   } catch (error) {
-    console.log(error.message)
-
-    return error.message.split("\n")[0]
+    // console.log(error.message)
+    return error.message.split("\n")[1]
   }
 }
 
 test("key checking corrects", async () => {
+  const keyFilePath = getKeyFilePath()
+  if (existsSync(keyFilePath)) {
+    await rm(keyFilePath)
+  }
+
   expect(cliErrorRun()).toMatchSnapshot()
 
-  expect(existsSync(join(cwd(), "key.json"))).toBe(true)
+  expect(existsSync(keyFilePath)).toBe(true)
 
   expect(cliErrorRun()).toMatchSnapshot()
 })
